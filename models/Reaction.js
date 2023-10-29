da493627f5315b2025fa4883f78c84d38f630242
Kiara Johnson <kiarajohnson91@gmail.com>
2023-10-28 19:53:07 -0500

    added models

---
+++ models/Reaction.js
@@ -0,0 +1,34 @@
+// Imports necessary modules from the 'mongoose' package
+const { Schema, Types } = ('mongoose');
+
+// Schema that creates a Reaction
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // TODO: Use a getter method to format the timestamp on query
+        },
+    },
+    {
+        toJSON: {
+          virtuals: true,
+        },
+        id: false,
+      }
+);
+
+module.exports = reactionSchema;
